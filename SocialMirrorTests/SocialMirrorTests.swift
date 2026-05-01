3ba7f4d9df3c91d8c0c9b25c489410d25fcd9b24
suthardivy183-lang <suthardivy183@gmail.com>
2026-05-01 20:14:01 +0530

    Initial Commit

---
+++ SocialMirrorTests/SocialMirrorTests.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMirrorTests.swift
+//  SocialMirrorTests
+//
+//  Created by SUTHAR DIVY DEVENDRABHAI on 01/05/26.
+//
+
+import Testing
+@testable import SocialMirror
+
+struct SocialMirrorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
